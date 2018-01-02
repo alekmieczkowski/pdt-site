191f200d1b82394f93136d2f188ee42d7c8a7fd6
amieczko <amieczko@uncc.edu>
2018-01-02 00:53:47 -0500

    Added footer, updated site style, started rush schedule section

--- themes/pdt-nce/assets/javascript/home.js
+++ themes/pdt-nce/assets/javascript/home.js
@@ -1,31 +1,28 @@
        
 function getData(){
-    $.request('onPillarsCall', {
-        success: function(data) {
-            return data;
-        }
-    });
+    
 }
 function toggle_pillar(pillar_name){
-    
-         //create id obj
-         var arr = $.map(getData(), function(el) { return el });
-        //set image for selected
-        $(pillar_name).attr("src",id[pillar_name][1]);
-        console.log(option_id[pillar_name][1]);
-    
-        //remove current ID from ID's
-        delete option_id[pillar_name];
-    
-        //remove overlay if exists
-        for(var i  in option_id){
-            $(i).attr("src",option_id[i][0]);
-            console.log(i);
+    $.request('onPillarsCall', {
+        success: function(data) {
+             //create id obj
+         var id = $.map(data, function(el) { return el });
+         console.log("id: "+id);
+         //set image for selected
+         $(pillar_name).attr("src",id[pillar_name][1]);
+         console.log(id[pillar_name][1]);
+     
+         //remove current ID from ID's
+         delete id[pillar_name];
+     
+         //remove overlay if exists
+         for(var i  in id){
+             $(i).attr("src",id[i][0]);
+             console.log(i);
+         }
         }
+    });
     
-    
-    
-        
     }
     
     $('#pillar-friendship').on('click',function(e){
