c2c8d50695b7fa7373be13a6f842f76ba0b68410
amieczko <amieczko@uncc.edu>
2018-01-07 18:18:33 -0500

    Added backend support for chapter Statistics

--- themes/pdt-nce/assets/javascript/nationals.js
+++ themes/pdt-nce/assets/javascript/nationals.js
@@ -1,6 +1,5 @@
 $("div.founder-text").click(function() {
-    //note than the selectors used here will result in somewhat odd behaviour if there 
-    //are multiple div.expand elements in the page :)
+
 
     if($(this).height() == 70){
         $(this).animate({
@@ -14,9 +13,6 @@ $("div.founder-text").click(function() {
         },800);
     }
     $(this).find("img.show-more").toggleClass("rotate").animate(200);
-   
-    //$("img.show-more").animate({
-    //    rotate : $("img.show-more").rotate(180)
-    //},800);
+
 
 });
