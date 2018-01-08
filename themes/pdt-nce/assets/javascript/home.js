b47561e36896fead00ed9ef99d35cc3be2a823dd
amieczko <amieczko@uncc.edu>
2018-01-07 21:16:28 -0500

    Added Transition effects for rush events, updated rush header, added rush image placeholder

--- themes/pdt-nce/assets/javascript/home.js
+++ themes/pdt-nce/assets/javascript/home.js
@@ -9,22 +9,19 @@ $( document ).ready(function() {
 
 var pillar_id= [".pillar-friendship",".pillar-rectitude", ".pillar-learning"];
 function toggle_pillar(pillar_name){
-    console.log("Toggle Pillar: "+pillar_name);
     var selected = pillar_name.replace('.','')+"-selected";
     //if button is not already selected
 
     if (!$(pillar_name).hasClass(selected)){
-        console.log("friendship button");
+
         //remove selection from others
         for (index = 0; index < pillar_id.length; ++index) {
             var pillar_arr = pillar_id[index].replace('.','')+"-selected";
                 if ($(pillar_id[index]).hasClass(pillar_arr)){
                     $(pillar_id[index]).removeClass(pillar_arr);
-                    console.log("pillar arrr: "+pillar_arr);
         }
         //make pillar selected
         $(pillar_name).addClass(selected);
-        console.log("Selected: "+selected);
 
     };
     
@@ -41,19 +38,16 @@ function toggle_text(pillar_name){
         $("#learning").fadeOut(400);
         $("#rectitude").fadeOut(400);
         $("#friendship").delay(delay).fadeIn(700);
-        console.log("friendship");
         break;
         case '#learning':
         $("#friendship").fadeOut(400);
         $("#rectitude").fadeOut(400);
         $("#learning").delay(delay).fadeIn(700);
-        console.log("learning");
         break;
         case '#rectitude':
         $("#learning").fadeOut(400);
         $("#friendship").fadeOut(400);
         $("#rectitude").delay(delay).fadeIn(700);
-        console.log("rectitude");
         break;
 
     }
@@ -61,19 +55,16 @@ function toggle_text(pillar_name){
     
     $('.pillar-friendship').on('click',function(e){
         toggle_pillar(".pillar-friendship");
-        console.log("post pillar");
         toggle_text('#pillar-friendship');
       });
     
       $('.pillar-learning').on('click',function(e){
         toggle_pillar('.pillar-learning');
-        console.log("post pillar");
         toggle_text('#pillar-learning');
       });
     
       $('.pillar-rectitude').on('click',function(e){
         toggle_pillar('.pillar-rectitude');
-        console.log("post pillar");
         toggle_text('#pillar-rectitude');
       });
 
@@ -82,16 +73,51 @@ function toggle_text(pillar_name){
 $("div#rush-item").click(function() {
         
     if($(this).find(".event-details").height() == 0){
-            $(this).find(".event-details").animate({
+
+        //opening div
+        $(this).find(".event-details").animate({
                 height : $(this).find(".event-details")[0].scrollHeight
-            },400);
-            $(this).find("img.event-arrow-img").rotate({ endDeg:90, duration:0.4, persist:true });
+            },800);
+        
+        //event date
+        $(this).find(".event-date").delay(400).animate({
+                opacity: 1
+            },200);
+        
+        //event description
+        $(this).find(".event-desc").delay(600).animate({
+            opacity: 1
+        },200);
+
+        //event location
+        $(this).find(".event-location").delay(700).animate({
+            opacity: 1
+        },200);
+
+        $(this).find("img.event-arrow-img").rotate({ endDeg:90, duration:0.4, persist:true });
             
         }
+        //closing div
         else{
-            $(this).find(".event-details").animate({
+            $(this).find(".event-details").delay(200).animate({
                 height : 0
-            },400);
+            },800);
+
+            //event location
+            $(this).find(".event-location").animate({
+                opacity: 0
+            },200);
+            //event description
+            $(this).find(".event-desc").delay(100).animate({
+                opacity: 0
+            },200);
+
+            //event date
+            $(this).find(".event-date").delay(400).animate({
+                opacity: 0
+            },200);
+
+            
             $(this).find("img.event-arrow-img").rotate({ endDeg:0, duration:0.4, persist:true });
         }
         
