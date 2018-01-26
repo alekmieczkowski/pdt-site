09ed74919737360d320601ef5739f44a859f1e7a
amieczko <amieczko@uncc.edu>
2018-01-26 00:03:31 -0500

    Added Google Calendar connection for upcoming events events home page

--- themes/pdt-nce/assets/javascript/events.js
+++ themes/pdt-nce/assets/javascript/events.js
@@ -30,4 +30,33 @@ $(document).ready(function(){
     $(".future-events-wrapper").hover(stop, loop); 
     
  
+
+
   });
+ /*Pull in google calendar information*/
+ var mykey = 'AIzaSyDSPiDsEFOFpKLXfl-Rf9VE8mhBVMM-1E0'; 
+ var calendarid = 'kdtollhp92mt19k0lton3rr12o@group.calendar.google.com'; 
+
+var cal = $.grabCalendar({
+	type: "events",
+    //clean_date: true,
+    maxEvents: 5
+});
+
+console.log(cal);
+console.log(cal[0]["summary"]);
+//create new calendar item in html for each event
+$.each(cal, function(index, event){
+    var div = document.createElement('div');
+    div.setAttribute('class', 'text-center');
+    div.setAttribute('id', 'future-item');
+    div.innerHTML = `
+        <h2><b>${event["summary"]}</b></h2>
+        <h3>${event["start"]}</h3>
+        <img class="future-btn img-responsive center-block" src="${img}" />
+    `;
+    
+    $('.future-events-wrapper').append(div);
+
+});
+
